3b0d81c0e3f28350e71a2eeaba546a072a29c801
ylyanovikova <ylyanovikova@ukr.net>
2022-06-08 14:06:17 +0200

    hw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import { useReducer, useRef } from "react";
+
+const initState = (init)=>{
+  return {cats: init, dogs:init}
+};
+
+const reducer = (state, action)=>{
+  switch(action.type){
+    case "cat":
+      return state.cats.push(action.cat)
+    case "dog":
+      return state.dogs.push(action.dog)
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, [], initState);
+  const catName= useRef(null);
+  const dogName= useRef(null);
 
+  const catSubmit = ()=>{
+    let cat = catName.current.value;
+    dispatch({type: "cat"})
+    console.log(cat);
+    console.log(state);
+
+  }
+  const dogSubmit = ()=>{
+    let dog = dogName.current.value;
+    dispatch({type:"dog"})
+    console.log(dog); 
+    console.log(state); 
+  }
 
   return (
     <div>
-      Hello
+      <form onSubmit={(e)=> e.preventDefault()}>
+        Cat: <input type="text" ref={catName} placeholder={"name"}/>
+        <button onClick={()=>catSubmit()}>Save Cat</button>
+      </form>
+
+      <form onSubmit={(e)=> e.preventDefault()}>
+        Dog: <input type="text" ref={dogName} placeholder="name"/>
+        <button onClick={()=>dogSubmit()}>Save dog</button>
+      </form>
     </div>
   );
 }
